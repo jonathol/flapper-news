5477cf866b641fb414c15c1278da5c670405e123
Jonathon Lin <jonathonlin93@yahoo.com>
2017-05-18 17:36:53 -0700

    add api routes and controllers

---
+++ flapper-news/app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  private
+
+  def create
+    post = Posts.find(params[:post_id])
+    comment = post.comments.create(comment_params)
+    respond_with post, comment
+  end
+
+  def upvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    respond_with post, comment
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
