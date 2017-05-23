1da38204f73ddff0492ec5358cfb95969efc100a
Jonathon Lin <jonathonlin93@yahoo.com>
2017-05-22 21:53:03 -0700

    fix post request for posts

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -5,13 +5,13 @@ angular.module('flapperNews')
   };
   o.getAll = function() {
     return $http.get('/posts.json').then(function(data){
-      console.log(data.data);
       angular.copy(data.data, o.posts);
     })
   };
   o.create = function(post) {
     return $http.post('/posts.json', post).then(function(data){
-      o.posts.push(data);
+      
+      o.posts.push(data.data);
     });
   };
   return o;
