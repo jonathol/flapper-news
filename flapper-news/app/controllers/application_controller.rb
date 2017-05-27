792d67678486770385a1a1715b0765d3e051de62
Jonathon Lin <jonathonlin93@yahoo.com>
2017-05-26 18:00:36 -0700

    add register and authenticate users

--- flapper-news/app/controllers/application_controller.rb
+++ flapper-news/app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
-class ApplicationController < ActionController::Base  
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   respond_to :json
 
-  def angular
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def angular
     render 'layouts/application'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
